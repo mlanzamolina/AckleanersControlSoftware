bb0c5ba5630440bb6b9999f350d9fc9b8d7ca2d2
WilmerFloresMolina <wilmerflores-tux2@unitec.edu>
2021-11-14 15:17:40 -0600

    login
    falta css y conectar a firebase

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,101 @@
 import React from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+
 export default function Login() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [user, setUser] = useState("");
+  const [pass, setPass] = useState("");
+
+
+
+  function handleSubmit() {
+      
+    console.log(pass);
+    console.log(user);
+
+    if (pass === null) {
+      alert("Ingrese Contraseña.");
+    } else {
+      if (pass === "") {
+        alert("Ingrese Contraseña.");
+      } else {
+        if (/^\w+([.-]?\w+)*@(?:|hotmail|outlook|yahoo|live|gmail)\.(?:|com|es)+$/.test(user)) {
+         
+          alert("bienvenido =)");
+          navigate("/");
+         
+        }else{
+          alert("Ingrese un correo.");
+        }
+      }
+    }
+   }
+
+   
     return (
-        <div className="cover">
-     
-      <h1>Login</h1>
-      <button
-        id="btn-2"
-        onClick={() => {
-            navigate("/");
-        }}
-      >
-        Login
-      </button>
-    </div>
-    )
-}
+      <div>
+      
+          
+            <h1>Iniciar Sesión</h1>
+            
+              <div className="email">
+                <label htmlFor="email">Correo Electrónico</label>
+                <input
+                  placeholder="Correo"
+                  type="email"
+                  name="email"
+                  noValidate
+                  onChange={(e) => { setUser(e.target.value); }}
+                />
+              </div>
+              <div className="password">
+                <label htmlFor="password">Contraseña</label>
+                <input
+                  placeholder="Contraseña"
+                  type="password"
+                  name="password"
+                  noValidate
+                  onChange={(e) => { setPass(e.target.value); }}
+                />
+              </div>
+              <div>
+                <small>
+                  <a
+                    className="clickable_text"
+                    onClick={() => {
+                      alert("hola1");
+                    }}
+                  >
+                    ¿Olvidó su contraseña?
+                  </a>
+                </small>
+              </div>
+              <div className="createAccount">
+                <button onClick={handleSubmit}>INICIAR SESIÓN</button>
+              </div>
+              <div>
+                <div>
+                  <small>
+                    ¿No tiene una cuenta?
+                    <a
+                      className="clickable_text"
+                      onClick={() => {
+                        alert("hola2");
+                       
+                      }}
+                    >
+                      {" "}
+                      Registrarse
+                    </a>
+                  </small>
+                </div>
+              </div>
+            
+      </div>
+    );
+  }
+    
+
+
