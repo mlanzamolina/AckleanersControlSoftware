2cd63d43ebba502923a7a9c91199201e0d326a8e
marco lanza <mlanzamolina@gmail.com>
2021-11-15 14:07:25 -0600

    landing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Home from './Home';
-import Login from './components/Login';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from './components/Home';
+//import Login from './components/Login';
 
 function App() {
   return (
     <>
-   
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+  
+     <Router>
+        <div>
+
+        <Switch>
+        <Route exact path="/">
+           <Home />
+        </Route>
+       {/** 
+        <Route path="/Login">
+        <Login />
+          </Route>*/
+}
+        </Switch>
+
+        </div>
+    </Router>
     </>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import Navigation from './navigation';
+import Header from './header';
+import Features from './features';
+import About from './about';
+import Gallery from './gallery';
+import Testimonials from './testimonials';
+import Team from './Team';
+import Contact from './contact';
+import data from '../data/data';
+import { Link } from 'react-router-dom';
+export class Home extends Component {
+ 
+  state = {
+    resumeData : {},
+    
+  }
+  componentDidMount(){
+    this.setState({resumeData: data});
+   
+  }
+
+  render() {
+    return (
+      
+      <div>
+         <div className="cover">
+      <h1>Home</h1>
+      <Link to="/Login">Login</Link>
+    </div>
+        <Navigation />
+        <Header data={this.state.resumeData.Header}/>
+        <Features data={this.state.resumeData.Features}/>
+        <About  data={this.state.resumeData.About}/>
+        <Gallery />
+        <Testimonials  data={this.state.resumeData.Testimonials}/>
+        <Team  data={this.state.resumeData.Team}/>
+        <Contact  data={this.state.resumeData.Contact}/>
+      </div>
+    )
+  }
+}
+
+export default Home
