8a8485afdaa87b44b2a41e27fa5806fa0f6f30d3
marco lanza <mlanzamolina@gmail.com>
2021-11-15 16:37:02 -0600

    vista de fotos arreglada

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -14,11 +14,10 @@ class Navigation extends Component {
           <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul className="nav navbar-nav navbar-right">
               <li><a href="#features" className="page-scroll">Servicios</a></li>
-              <li><a href="#about" className="page-scroll">About</a></li>
-              <li><a href="#portfolio" className="page-scroll">Gallery</a></li>
-              <li><a href="#testimonials" className="page-scroll">Testimonials</a></li>
-              <li><a href="#team" className="page-scroll">Team</a></li>
-              <li><a href="#contact" className="page-scroll">Contact</a></li>
+              <li><a href="#about" className="page-scroll">SOBRE NOSOTROS</a></li>
+              <li><a href="#portfolio" className="page-scroll">Fotos</a></li>
+              <li><a href="#testimonials" className="page-scroll">Reviews</a></li>
+              <li><a href="#contact" className="page-scroll">Contactenos</a></li>
               <Link id="link" to="/Login">Login</Link>
             </ul>
           </div>
