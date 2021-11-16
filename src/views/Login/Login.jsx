0b09724666eaeaa6f03e9e9f463bcd66f641c583
marco lanza <mlanzamolina@gmail.com>
2021-11-16 15:14:22 -0600

    arreglado css de login y formulario empleario

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -2,94 +2,114 @@ import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Login.css";
+import logo from "../../img/logo.png"
 
 export default function Login() {
   const [user, setUser] = useState("");
   const [pass, setPass] = useState("");
 
   function handleSubmit() {
+      
     console.log(pass);
     console.log(user);
 
-    if (pass === null) {
-      alert("Ingrese Contraseña.");
-    } else {
-      if (pass === "") {
-        alert("Ingrese Contraseña.");
-      } else {
-        if (
-          /^\w+([.-]?\w+)*@(?:|hotmail|outlook|yahoo|live|gmail)\.(?:|com|es)+$/.test(
-            user
-          )
-        ) {
-          alert("bienvenido =");
-        } else {
+    
+      
+        if (/^\w+([.-]?\w+)*@(?:|hotmail|outlook|yahoo|live|gmail)\.(?:|com|es)+$/.test(user)) {
+          if (pass === null) {
+            alert("Ingrese Contraseña.");
+          } else {
+            if (pass === "") {
+              alert("Ingrese Contraseña.");
+            }else{
+              alert("bienvenido =)");
+            }
+          }
+
+         
+        }else{
           alert("Ingrese un correo.");
         }
-      }
-    }
-  }
+      
+    
+   }
 
-  return (
-    <div>
+   
+    return (
+      <>
+       <a href="/"><img src={logo} alt="logo ackleaners" width="250" style={{
+              margin: 0,
+              top: "auto",
+              right: 45,
+              top: 40,
+              position: "fixed",
+            }}/></a> 
+     
+      <div className="wrapper">
+      <div className="form-wrapper">
       <h1>Iniciar Sesión</h1>
+      <form >
+            <div className="login-form">
+              <div className="email">
+                <label htmlFor="email">Correo Electrónico</label>
+                <input
+                  placeholder="Correo"
+                  type="email"
+                  name="email"
+                  noValidate
+                  onChange={(e) => { setUser(e.target.value); }}
+                />
+              </div>
+              <div className="password">
+                <label htmlFor="password">Contraseña</label>
+                <input
+                  placeholder="Contraseña"
+                  type="password"
+                  name="password"
+                  noValidate
+                  onChange={(e) => { setPass(e.target.value); }}
+                />
+              </div>
+              <div>
+                <small>
+                  <a
+                    className="clickable_text"
+                    onClick={() => {
+                      alert("hola1");
+                    }}
+                  >
+                    ¿Olvidó su contraseña?
+                  </a>
+                </small>
+              </div>
+              <div className="createAccount">
+                <button onClick={handleSubmit}>INICIAR SESIÓN</button>
+              </div>
+              <div>
+                <div>
+                  <small>
+                    ¿No tiene una cuenta?
+                    <a
+                      className="clickable_text"
+                      onClick={() => {
+                        alert("hola2");
+                       
+                      }}
+                    >
+                      {" "}
+                      Registrarse
+                    </a>
+                  </small>
+                </div>
+              </div>
+            
+              </div>
+              </form> 
+      </div>
+      </div>
+      </>
+    );
+  }
+    
 
-      <div className="email">
-        <label htmlFor="email">Correo Electrónico</label>
-        <input
-          placeholder="Correo"
-          type="email"
-          name="email"
-          noValidate
-          onChange={(e) => {
-            setUser(e.target.value);
-          }}
-        />
-      </div>
-      <div className="password">
-        <label htmlFor="password">Contraseña</label>
-        <input
-          placeholder="Contraseña"
-          type="password"
-          name="password"
-          noValidate
-          onChange={(e) => {
-            setPass(e.target.value);
-          }}
-        />
-      </div>
-      <div>
-        <small>
-          <a
-            className="clickable_text"
-            onClick={() => {
-              alert("hola1");
-            }}
-          >
-            ¿Olvidó su contraseña?
-          </a>
-        </small>
-      </div>
-      <div className="createAccount">
-        <button onClick={handleSubmit}>INICIAR SESIÓN</button>
-      </div>
-      <div>
-        <div>
-          <small>
-            ¿No tiene una cuenta?
-            <a
-              className="clickable_text"
-              onClick={() => {
-                alert("hola2");
-              }}
-            >
-              {" "}
-              Registrarse
-            </a>
-          </small>
-        </div>
-      </div>
-      <Link to="/">Home</Link>
-    </div>
-  );
-}
+
