a39c2f7c7d1b493faec35481421d32bbc9553c0b
marco lanza <mlanzamolina@gmail.com>
2021-11-16 11:03:38 -0600

    whatsapp button terminado

---
+++ src/components/whatsapp.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Fab from "@material-ui/core/Fab";
+
+class Whatsapp extends Component {
+  render() {
+    return (
+      <>
+        <div className="whatsapp">
+          <Fab
+            color="primary"
+            aria-label="whatsapp"
+            style={{
+              margin: 0,
+              top: "auto",
+              right: 40,
+              bottom: 40,
+              left: "auto",
+              position: "fixed",
+            }}
+          >
+            <a
+              id="whatsapp"
+              href="https://web.whatsapp.com/send?phone=+50432868726"
+              target="_blank"
+            >
+              {" "}
+              <img
+                src="https://1000marcas.net/wp-content/uploads/2019/11/WhatsApp-logo.png"
+                alt="whatsapp"
+                height="57"
+              />{" "}
+            </a>
+          </Fab>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Whatsapp;
