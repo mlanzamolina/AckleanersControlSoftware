b632bf3ce92e029760cd26bb708d3b18c5656fea
rodrigobardales <rodrigo.bardales@hotmail.com>
2021-11-16 18:03:00 -0600

    Vista administrativa
    vista administrativa con side menu

---
+++ src/views/Management/SideBarData.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+
+export const SidebarData = [
+  {
+    title: 'Home',
+    path: '/',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Reportes',
+    path: '/reportes',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Cuadrillas',
+    path: '/cuadrillas',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Inventarios',
+    path: '/inventarios',
+    cName: 'nav-text'
+  },
+];
