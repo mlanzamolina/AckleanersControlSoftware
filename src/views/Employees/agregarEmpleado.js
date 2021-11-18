ab9fb85e81f8857b026fdb440d5b9e96e8a63e6e
Marco Lanza <mlanzamolina@gmail.com>
2021-11-18 17:44:48 -0600

    opciones

--- src/views/Employees/agregarEmpleado.js
+++ src/views/Employees/agregarEmpleado.js
@@ -1,17 +1,17 @@
 import React, { Fragment, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../img/logo.png";
-import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
-import { SidebarData } from './SideBarData';
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import { SidebarData } from "./SideBarData";
 import { dbEmpleado } from "../../components/firebase";
-import { collection, addDoc } from 'firebase/firestore'
+import { collection, addDoc } from "firebase/firestore";
 
 const AgregarEmpleado = () => {
   const [sidebar, setSidebar] = useState(false);
   const tablaEmpleadosRef = collection(dbEmpleado, "meterEmpleados");
   const showSidebar = () => setSidebar(!sidebar);
-  
+
   const [dats, setDatos] = useState({
     nombre: " ",
     id: " ",
@@ -25,47 +25,54 @@ const AgregarEmpleado = () => {
       [event.target.name]: event.target.value,
     });
   };
-  
-  const handleSubmit = async (e) =>{
-    await addDoc(tablaEmpleadosRef,{nombre: dats.nombre, 
-      id: dats.id, numero: dats.numero, correo: dats.correo});
-  };
 
+  const handleSubmit = async (e) => {
+    await addDoc(tablaEmpleadosRef, {
+      nombre: dats.nombre,
+      id: dats.id,
+      numero: dats.numero,
+      correo: dats.correo,
+    });
+  };
 
   return (
     <>
-    <div className="dropdown" style={{float: 'right',}}>
-  <button class="dropbtn">Opciones</button>
-  <div class="dropdown-content">
-  <a href="/ListarEmpleado">Listar Empleado</a>
-  <a href="/AgregarEmpleado">AgregarEmpleado</a>
-  <a href="/ModificarEmpleado">ModificarEmpleado</a>
-  </div>
-</div>
-    <Fragment>
-      <a href="/">
-        <img
-          src={logo}
-          alt="logo ackleaners"
-          width="250"
-          style={{
-            margin: 0,
-            top: "auto",
-            right: 45,
-            bottom: 40,
-            position: "fixed",
-          }}
-        />
-      </a>
-      <div className='managementsidemenu'>
-          <Link to='#' className='managementmenu-bars'>
+      <div className="dropdown" style={{ float: "right" }}>
+        <button class="dropbtn">Opciones</button>
+        <div class="dropdown-content">
+          <a href="/ListarEmpleado">Listar Empleado</a>
+          <a href="/AgregarEmpleado">AgregarEmpleado</a>
+          <a href="/ModificarEmpleado">ModificarEmpleado</a>
+        </div>
+      </div>
+      <Fragment>
+        <a href="/">
+          <img
+            src={logo}
+            alt="logo ackleaners"
+            width="250"
+            style={{
+              margin: 0,
+              top: "auto",
+              right: 45,
+              bottom: 40,
+              position: "fixed",
+            }}
+          />
+        </a>
+        <div className="managementsidemenu">
+          <Link to="#" className="managementmenu-bars">
             <FaIcons.FaBars onClick={showSidebar} />
           </Link>
         </div>
-        <nav className={sidebar ? 'managementnav-menu active' : 'managementnav-menu'}>
-          <ul className='managementnav-menu-items' onClick={showSidebar}>
-            <li className='navbar-toggle'>
-              <Link to='#' className='managementmenu-bars'>
+        <nav
+          className={
+            sidebar ? "managementnav-menu active" : "managementnav-menu"
+          }
+        >
+          <ul className="managementnav-menu-items" onClick={showSidebar}>
+            <li className="navbar-toggle">
+              <Link to="#" className="managementmenu-bars">
                 <AiIcons.AiOutlineClose />
               </Link>
             </li>
@@ -81,70 +88,72 @@ const AgregarEmpleado = () => {
             })}
           </ul>
         </nav>
-      <div>
-        <h1 className="tituloh1">Registro Empleado</h1>
-      </div>
+        <div>
+          <h1 className="tituloh1">Registro Empleado</h1>
+        </div>
 
-      <form className="col-md" >
-        <div >
-          <h3>Nombre Completo: </h3>
-          <input
-            placeholder="Ingrese Nombre"
-            className="form-control"
-            name="nombre"
-            onChange={handleInputChance}
-            required
-          ></input>
-        </div>
-        <div>
-          <h3>ID: </h3>
-          <input
-            placeholder="Ingrese número de identidad"
-            className="form-control"
-            type="text"
-            name="id"
-            onChange={handleInputChance}
-            required
-          ></input>
-        </div>
-        <div>
-          <h3>No. Contacto: </h3>
-          <input
-            placeholder="Ingrese número de Telefono/celular"
-            className="form-control"
-            type="text"
-            name="numero"
-            onChange={handleInputChance}
-            required
-          ></input>
-        </div>
-        <div>
-          <h3>Correo Electronico: </h3>
-          <input
-            placeholder="ejemplo.123@ejemplo.com"
-            className="form-control"
-            type="text"
-            name="correo"
-            onChange={handleInputChance} 
-            required
-          ></input>
-        </div>
-        <div>
-          <Link to="/">
-            <button type="submit" className="btn btn-danger">
-              Cancelar
+        <form className="col-md">
+          <div>
+            <h3>Nombre Completo: </h3>
+            <input
+              placeholder="Ingrese Nombre"
+              className="form-control"
+              name="nombre"
+              onChange={handleInputChance}
+              required
+            ></input>
+          </div>
+          <div>
+            <h3>ID: </h3>
+            <input
+              placeholder="Ingrese número de identidad"
+              className="form-control"
+              type="text"
+              name="id"
+              onChange={handleInputChance}
+              required
+            ></input>
+          </div>
+          <div>
+            <h3>No. Contacto: </h3>
+            <input
+              placeholder="Ingrese número de Telefono/celular"
+              className="form-control"
+              type="text"
+              name="numero"
+              onChange={handleInputChance}
+              required
+            ></input>
+          </div>
+          <div>
+            <h3>Correo Electronico: </h3>
+            <input
+              placeholder="ejemplo.123@ejemplo.com"
+              className="form-control"
+              type="text"
+              name="correo"
+              onChange={handleInputChance}
+              required
+            ></input>
+          </div>
+          <div>
+            <Link to="/">
+              <button type="submit" className="btn btn-danger">
+                Cancelar
+              </button>
+            </Link>
+
+            <button
+              type="submit"
+              className="btn btn-primary"
+              onClick={handleSubmit}
+            >
+              Registrar
             </button>
-          </Link>
-
-          <button type="submit" className="btn btn-primary" onClick={handleSubmit}>
-            Registrar
-          </button>
-        </div>
-      </form>
-      {/* <h3>{dats.nombre}-{dats.id}-{dats.numero}-{dats.correo}</h3> */}
-      
-    </Fragment>
-    
+          </div>
+        </form>
+        {/* <h3>{dats.nombre}-{dats.id}-{dats.numero}-{dats.correo}</h3> */}
+      </Fragment>
     </>
   );
 };
