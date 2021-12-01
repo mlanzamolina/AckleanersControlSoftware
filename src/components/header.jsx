7204c7ec24562379ceb9b61376e67d0b40237ef3
Marco Lanza <mlanzamolina@gmail.com>
2021-12-01 14:14:34 -0600

    contactanos responsive y funciona

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,8 +7,8 @@ export class Header extends Component {
      <>
    <div id="headerm">
        <img src={about} alt="" width= "100%"/>
-       <div class="centered" ><a href="#features" className="myButtonm"><h2>Nuestros servicios</h2></a></div>
-       <div class="centered2"><a href="#contact" className="myButtonm"><h2>Contactenos</h2></a></div>
+       <div className="centered" ><a href="#features" className="myButtonm"><h2>Nuestros servicios</h2></a></div>
+       <div className="centered2"><a href="#contact" className="myButtonm"><h2>Contactenos</h2></a></div>
        </div>
      </>
     );
--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -10,12 +10,36 @@ export class testimonials extends Component {
           <br />  <br />  <br />
             <h2>Reseñas</h2>  <br />  <br />  <br />
           </div>
-          <div className="row">
+          <div className="row  justify-content-center">
   
-          <a class="btn btn-info" href="https://g.page/r/CVrdXIDa8X2zEAI/review" role="button"><i className="fa fa-hand-o-right" /> Dejanos una reseña <i className="fa fa-hand-o-left" /></a>
+          <a className="btn btn-info" href="https://g.page/r/CVrdXIDa8X2zEAI/review" role="button"><i className="fa fa-hand-o-right" /> Dejanos una reseña <i className="fa fa-hand-o-left" /></a>
         
           </div>
           <br />  <br />  <br />
+          <div className="centered3">
+          <a
+          target="_blank"
+          style={{}}
+          href="https://www.facebook.com/Ackleaners-106521830930162"
+        >
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Facebook_icon.svg/1200px-Facebook_icon.svg.png"
+            width="90vw"
+            alt="..."
+          />
+        </a>
+        <a
+          target="_blank"
+          style={{}}
+          href="https://www.instagram.com/ackleaners/"
+        >
+          <img
+            src="https://aegean600.com/wp-content/uploads/instagram-logo.png"
+            width="90vw"
+            alt="..."
+          />
+        </a>
+        </div>
         </div>
       </div>
 </>
