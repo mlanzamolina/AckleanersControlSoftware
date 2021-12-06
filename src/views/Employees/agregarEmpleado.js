5780311d296e0e430ca968f745e9edfc476cf58a
rodrigobardales <rodrigo.bardales@hotmail.com>
2021-12-05 18:57:02 -0600

    New navigation
    Nueva implemenatacion de navbar para los componentes y reduccion de lineas de codigo en general

--- src/views/Employees/agregarEmpleado.js
+++ src/views/Employees/agregarEmpleado.js
@@ -7,6 +7,7 @@ import { SidebarData } from "./SideBarData";
 import { dbEmpleado } from "../../components/firebase";
 import { collection, addDoc } from "firebase/firestore";
 import swal from "sweetalert";
+import EmployeeNavigation from "./EmployeeNavigation";
 
 const AgregarEmpleado = () => {
   const [sidebar, setSidebar] = useState(false);
@@ -58,57 +59,7 @@ const AgregarEmpleado = () => {
 
   return (
     <>
-      <div className="dropdown" style={{ float: "right" }}>
-        <button class="dropbtn">Opciones</button>
-        <div class="dropdown-content">
-          <a href="/ListarEmpleado">Listar Empleado</a>
-          <a href="/AgregarEmpleado">AgregarEmpleado</a>
-          <a href="/ModificarEmpleado">ModificarEmpleado</a>
-        </div>
-      </div>
-      <Fragment>
-        <a href="/">
-          <img
-            src={logo}
-            alt="logo ackleaners"
-            width="250"
-            style={{
-              margin: 0,
-              top: "auto",
-              right: 45,
-              bottom: 40,
-              position: "fixed",
-            }}
-          />
-        </a>
-        <div className="managementsidemenu">
-          <Link to="#" className="managementmenu-bars">
-            <FaIcons.FaBars onClick={showSidebar} />
-          </Link>
-        </div>
-        <nav
-          className={
-            sidebar ? "managementnav-menu active" : "managementnav-menu"
-          }
-        >
-          <ul className="managementnav-menu-items" onClick={showSidebar}>
-            <li className="navbar-toggle">
-              <Link to="#" className="managementmenu-bars">
-                <AiIcons.AiOutlineClose />
-              </Link>
-            </li>
-            {SidebarData.map((item, index) => {
-              return (
-                <li key={index} className={`management${item.cName}`}>
-                  <Link to={item.path}>
-                    {item.icon}
-                    <span>{item.title}</span>
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
+      <EmployeeNavigation></EmployeeNavigation>
         <div>
           <h1 className="tituloh1">Registro Empleado</h1>
         </div>
@@ -178,7 +129,6 @@ const AgregarEmpleado = () => {
             </button>
           </div>
         </form>
-      </Fragment>
     </>
   );
 };
