68bb1a814eac68ec1fcc1e0d7f659df05502b635
Oscar Luis Sanchez <tinocoscar11@unitec.edu>
2022-02-24 13:47:25 -0600

    Done new frontend

--- src/views/Reports/ConfirmReport.jsx
+++ src/views/Reports/ConfirmReport.jsx
@@ -14,7 +14,7 @@ import { dbOrdenes, db } from "../../components/firebase";
 import swal from "sweetalert";
 
 function ConfirmReport() {
-  const history= useHistory();
+  const history = useHistory();
   const [data, setData] = useState([]);
   let { id } = useParams();
   const [ordenes] = useCollectionData(collection(db, "OrdenesTrabajo"), {
@@ -41,63 +41,104 @@ function ConfirmReport() {
       .catch((e) => {
         alert(e);
       });
-      history.push("/Management");
+    history.push("/Management");
   };
 
   return (
     <>
       <Nav />
       <div className="contentf">
-        {documentos
-          ? documentos.map((item, index) => {
+        <h1 style={{
+          width: "100%",
+          textAlign: "center",
+          marginTop: "1%",
+          marginBottom: "80px",
+          borderBottom: "2px solid black",
+          fontSize: "30px"
+        }}
+        >Confirmar Reporte de Orden de Trabajo</h1>
+        <div className="container rounded contenedorFormulario" style={{ height: "70%" }}>
+          <label className="letrasFormularioReportes" style={{ marginTop: "30px" }} classname="letrasFormulario">
+            Hola Equipo de Ackleaners,
+            <br />
+            <br /> Se ha registrado un nuevo reporte para una orden de trabajo
+            por favor, verificar que los datos esten correctos.
+            <br></br>
+            <br />
+          </label>
+          <br />
+          {documentos
+            ? documentos.map((item, index) => {
               if (item.idreporte === id) {
                 //download a pdf of reports to review
+
                 return (
-                  <tr key={item.id} className="text-center">
-                    <td className="table-primary">{item.nombre}/ </td>
-                    <td className="table-primary">{id}/ </td>
-                    <td className="table-primary">
+                  <Fragment>
+                    <label key={item.id} className="letrasFormularioReportes">Título Reporte: {item.nombre} </label>
+                    <br />
+                    <label key={item.id} style={{ marginTop: "20px" }} className="letrasFormularioReportes">
+                      ID de Reporte: {id} </label>
+                    <br />
+                    <label key={item.id} style={{ marginTop: "20px" }} className="letrasFormularioReportes">
+                      Fecha de Reporte: {item.fecha} </label>
+                    <br />
+                    <label key={item.id} style={{ marginTop: "20px" }} className="letrasFormularioReportes">
+                      Descripción del Reporte: {item.descripcion} </label>
+
+                    <br />
+                    <label key={item.id} className="letrasFormularioReportes" style={{ marginTop: "20px" }}>Ver Reporte:
                       <a href={item.url} target="_blank">
-                        <button type="button" class="btn btn-primary">
-                          {" "}
-                          Download
+                        <button type="button"
+                          className="btn btn-primary"
+                          style={{ marginLeft: "2%" }}>
+                          Descargar
                         </button>
                       </a>
-                    </td>
-                  </tr>
+                    </label>
+                  </Fragment>
                 );
               }
-            })
-          : null}
 
-        
-        <button
-          type="button"
-          className="btn btn-success"
-          onClick={handleSubmit}
-          style={{
-            fontSize: "20px",
-            paddingLeft: "110px",
-            paddingRight: "110px",
-          }}
-        >
-          Accept: enviar a cliente reporte finalizado
-        </button>
-        <a href="/CrearReportes">
-          {" "}
-          <button
-            type="button"
-            className="btn btn-danger"
-            style={{
-              fontSize: "20px",
-              paddingLeft: "110px",
-              paddingRight: "110px",
-            }}
-          >
-            Deny: volver a hacer el reporte
-          </button>
-        </a>
+            })
+            : null}
+
+          <div class="col-12 offset-lg-1">
+            <button
+              type="button"
+              className="btn btn-success"
+              onClick={handleSubmit}
+              style={{
+                fontSize: "20px",
+                paddingLeft: "8%",
+                paddingRight: "8%",
+                marginTop: "6%"
+
+              }}
+            >
+              Completar, Enviar Reporte a Cliente
+
+            </button>
+            <a href="/CrearReportes">
+              {" "}
+              <button
+                type="button"
+                className="btn btn-danger"
+                style={{
+                  fontSize: "20px",
+                  paddingLeft: "8%",
+                  paddingRight: "8%",
+                  marginLeft: "2%",
+                  marginTop: "6%"
+                }}
+              >
+                Denegar, Rehacer el Reporte
+              </button>
+
+            </a>
+          </div>
+        </div>
       </div>
+
     </>
   );
 }
