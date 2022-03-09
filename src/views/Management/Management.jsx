104beacbab3b5cc80b67649afeee4ee5f8c787b9
Oscar Luis Sanchez <tinocoscar11@unitec.edu>
2022-03-08 22:18:25 -0600

    Boton en tabla de recordatorios
    Home

--- src/views/Management/Management.jsx
+++ src/views/Management/Management.jsx
@@ -1,12 +1,8 @@
 import React, { useState, useEffect, Fragment } from "react";
 import { auth } from "../../components/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
-import * as FaIcons from "react-icons/fa";
-import * as AiIcons from "react-icons/ai";
 import { Link } from "react-router-dom";
-import { SidebarData } from "./SideBarData";
 import "./Management.css";
-import logo from "../../img/logo.png";
 import Nav from "../NavAdmin";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import { collection, connectFirestoreEmulator, doc, Firestore, updateDoc } from "firebase/firestore";
@@ -29,17 +25,17 @@ import swal from "sweetalert";
 function SideMenu() {
 
   function removeDuplicates(arr) {
-    return arr.filter((item, 
-        index) => arr.indexOf(item) === index);
-}
+    return arr.filter((item,
+      index) => arr.indexOf(item) === index);
+  }
 
-  function constructstring(){
+  function constructstring() {
     let cadena = "";
-   for (let index = 0; index < arrayClientes.length/2; index++) {
-    cadena += "[ Nombre: " + arrayClientes[index] + " Telefono: " + arrayClientestel[index] + " ]";
-   }
-   return cadena;
- }
+    for (let index = 0; index < arrayClientes.length / 2; index++) {
+      cadena += "[ Nombre: " + arrayClientes[index] + " Telefono: " + arrayClientestel[index] + " ]";
+    }
+    return cadena;
+  }
 
   async function handleSend() {
     var template_params = {
@@ -48,7 +44,7 @@ function SideMenu() {
       message: constructstring(),
     };
     console.log(template_params);
-  
+
     emailjs
       .send(
         "service_07irwdr",
@@ -59,7 +55,7 @@ function SideMenu() {
       .then(
         swal({
           title: "¡Enviado!",
-          text: "Se envio correo de recordatorio",
+          text: "Correo electronico enviado con recordatorios pendientes",
           icon: "info",
           button: "Aceptar",
         })
@@ -93,21 +89,21 @@ function SideMenu() {
   const [fechaactual, setfechaactual] = useState([]);
   const [ventanaConfirm, setVentanaConfirm] = useState(false);
   const [recordatorio, setRecordatorio] = useState(false);
-  const [id,SetID] = useState("");
+  const [id, SetID] = useState("");
 
 
 
-  const obtenerID=(id2)=>{
-  SetID(id2);
-  console.log(id);
+  const obtenerID = (id2) => {
+    SetID(id2);
+    console.log(id);
   }
 
 
-  const modificarRecordatorio= async (e)=>{
+  const modificarRecordatorio = async (e) => {
     e.preventDefault();
     console.log(id);
     const record = doc(db, "OrdenesTrabajo", id);
-  
+
     await updateDoc(record, {
       recordad: recordatorio
     }).catch((error) => {
@@ -131,9 +127,9 @@ function SideMenu() {
   }
 
   const editRow = (id2) => {
-   SetID(id2);
-   setRecordatorio(true);
-   console.log(id);
+    SetID(id2);
+    setRecordatorio(true);
+    console.log(id);
     if (ventanaConfirm === false) {
       setVentanaConfirm(!ventanaConfirm);
     } else {
@@ -153,7 +149,7 @@ function SideMenu() {
   return (
     <>
       <Nav />
-      
+
       <Fragment>
         <div class="contentf">
           <h1
@@ -168,13 +164,13 @@ function SideMenu() {
             Hola, {userName}{" "}
           </h1>
           <h2
-          style={{
-            width: "100%",
-            textAlign: "center",
-            marginTop: "1%",
-            borderBottom: "2px solid black",
-            fontSize:"25px"
-          }}>
+            style={{
+              width: "100%",
+              textAlign: "center",
+              marginTop: "1%",
+              borderBottom: "2px solid black",
+              fontSize: "25px"
+            }}>
             Resumen de Servicios Pendientes</h2>
           <table className="table table-dark table-striped" align="center">
             {/*<table className="ta" align="center">*/}
@@ -192,105 +188,116 @@ function SideMenu() {
             <tbody>
               {clientes
                 ? clientes.map((item, index) => {
-                    if (item.proxima_revision !== ""  && !item.recordad) {
-                      //fecha dividir en un arreglo pos 0=dia pos 1=mes pos 2=año
-                      const fechArr = item.proxima_revision.split("/");
-                      //fecha actual dividir en un arreglo pos 0=dia pos 1=mes pos 2=año
-                      var pDate = new Date(
-                        fechArr[2],
-                        fechArr[1] - 1,
-                        fechArr[0]
+                  if (item.proxima_revision !== "" && !item.recordad) {
+                    //fecha dividir en un arreglo pos 0=dia pos 1=mes pos 2=año
+                    const fechArr = item.proxima_revision.split("/");
+                    //fecha actual dividir en un arreglo pos 0=dia pos 1=mes pos 2=año
+                    var pDate = new Date(
+                      fechArr[2],
+                      fechArr[1] - 1,
+                      fechArr[0]
+                    );
+                    //probando formato de fecha
+                    //console.log(pDate.getDate() + "/" + (pDate.getMonth() + 1) + "/" + pDate.getFullYear());
+                    //fecha actual igual a 7 dias o menos de la proxima revision
+                    pDate = new Date(
+                      pDate.getFullYear(),
+                      pDate.getMonth(),
+                      pDate.getDate() - 7
+                    );
+                    //enviar recordatorio aqui
+                    if (fechaactual.getTime() >= pDate.getTime()) {
+                      arrayClientes.push(item.nombre);
+                      arrayClientestel.push(item.numero_telefono);
+                      return (
+                        <tr key={item.id} className="text-center">
+                          <td className="table-primary">{item.nombre}</td>
+                          <td className="table-primary">
+                            {item.numero_telefono}
+                          </td>
+                          <td className="table-primary">
+                            {item.tipo_vivienda}
+                          </td>
+                          <td className="table-primary">
+                            {item.proxima_revision}
+                          </td>
+
+                          <td class="table-primary">
+                            <div
+                              class="btn-group"
+                              role="group"
+                              aria-label="Basic example"
+                            >
+                              <Button
+                                onClick={() => editRow(item.id)}
+
+
+                                color="success">
+                                <i class="bi bi-card-checklist"></i>
+                              </Button>
+
+                            </div>
+                          </td>
+
+
+
+                        </tr>
                       );
-                      //probando formato de fecha
-                      //console.log(pDate.getDate() + "/" + (pDate.getMonth() + 1) + "/" + pDate.getFullYear());
-                      //fecha actual igual a 7 dias o menos de la proxima revision
-                      pDate = new Date(
-                        pDate.getFullYear(),
-                        pDate.getMonth(),
-                        pDate.getDate() - 7
-                      );
-                      //enviar recordatorio aqui
-                      if (fechaactual.getTime() >= pDate.getTime()) {
-                        arrayClientes.push(item.nombre);
-                        arrayClientestel.push(item.numero_telefono);
-                        return (
-                          <tr key={item.id} className="text-center">
-                            <td className="table-primary">{item.nombre}</td>                            
-                            <td className="table-primary">
-                              {item.numero_telefono}
-                            </td>
-                            <td className="table-primary">
-                              {item.tipo_vivienda}
-                            </td>
-                            <td className="table-primary">
-                              {item.proxima_revision}
-                            </td>
-
-                            <td class="table-primary">
-                      <div
-                        class="btn-group"
-                        role="group"
-                        aria-label="Basic example"
-                      >
-                        <Button
-                          onClick={()=>editRow(item.id)}
-                         
-                          
-                          color="success">
-                        <i class="bi bi-card-checklist"></i>
-                        </Button> 
-
-                        </div>
-                        </td>
-
-                            
-                            
-                          </tr>
-                        );
-                      }
                     }
-                  })
+                  }
+                })
                 : null}
             </tbody>
           </table>
           <Modal isOpen={ventanaConfirm}>
-          <ModalHeader closeButton>¿DESEA CONFIRMAR LA ORDEN DE TRABAJO?</ModalHeader>
+            <ModalHeader closeButton>¿DESEA CONFIRMAR LA ORDEN DE TRABAJO?</ModalHeader>
 
-          <ModalFooter>
-            <Button
-              onClick={(e)=>modificarRecordatorio(e)}
-              type="button"
-              variant="primary"
-              style={{background:"red"}}
+            <ModalFooter>
+              <Button
+                onClick={(e) => modificarRecordatorio(e)}
+                type="button"
+                variant="primary"
+                style={{ background: "red" }}
+              >
+                SI
+              </Button>
+
+              <Button
+                onClick={() => setVentanaConfirm(false)}
+
+                type="button"
+                variant="secondary"
+                style={{ background: "rgb(70,130,180)" }}
+              >
+                NO
+              </Button>
+            </ModalFooter>
+          </Modal>
+
+          <div className="col-12 offset-lg-4">
+            <button
+              type="submit"
+              class="btn btn-success"
+              style={{
+                marginBottom: "5%",
+                marginLeft:"4%",
+                paddingLeft:"10%",
+                paddingRight:"10%",
+                marginTop: "1%",
+                fontSize: "16px",
+              }}
+              onClick={handleSend}
             >
-              SI
-            </Button>
+              Enviar Lista
+            </button>
+          </div>
 
-            <Button
-              onClick={() =>setVentanaConfirm(false)}
-              
-              type="button"
-              variant="secondary"
-              style={{background:"rgb(70,130,180)"}}
-            >
-              NO
-            </Button>
-          </ModalFooter>
-        </Modal>
-
-        <button className="rounded botonSize sendMail" style={{width: '100%',
-      height: '20%',
-      justifyContent: 'center',
-      alignItems: 'center',
-      }} onClick={handleSend} >
-                      Enviar Lista
-          </button>
 
           <Link to="/adminOrders"></Link>
         </div>
       </Fragment>
 
-      
+
     </>
   );
 }
