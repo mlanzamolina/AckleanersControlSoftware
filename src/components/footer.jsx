e5d8dacf39a51df05930e95c8160c59437a51f49
Marco Lanza <mlanzamolina@gmail.com>
2022-03-23 18:28:56 -0600

    centrado nicely

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -28,7 +28,13 @@ export class footer extends Component {
           <div className="container mx-auto px-4">
             <div className="flex flex-wrap text-center lg:text-left">
               <div className="w-full lg:w-6/12 px-4">
-                <h4 className="text-3xl font-semibold">
+                
+              </div>
+            </div>
+           
+            <div className="flex flex-wrap items-center md:justify-between justify-center">
+              <div className="w-full md:w-4/12 px-4 mx-auto text-center">
+              <h4 className="text-3xl font-semibold">
                   ¡Mantente en contacto!
                 </h4>
                 <h5 className="text-lg mt-0 mb-2 text-blueGray-600">
@@ -38,11 +44,7 @@ export class footer extends Component {
                   <SocialIcon url="https://www.facebook.com/Ackleaners-106521830930162" style={{ height: 50, width: 50 }}/>
                   <SocialIcon url="https://www.instagram.com/ackleaners/" style={{ height: 50, width: 50 }}/>
                 </div>
-              </div>
-            </div>
-            <hr className="my-6 border-blueGray-300" />
-            <div className="flex flex-wrap items-center md:justify-between justify-center">
-              <div className="w-full md:w-4/12 px-4 mx-auto text-center">
+                <hr width="100%" className="my-6 border-blueGray-300" />
                 <div className="text-sm text-blueGray-500 font-semibold py-1">
                   Copyright © {new Date().getFullYear()} ACkleaners by{" "}
                   <a>ACkleaners</a>
